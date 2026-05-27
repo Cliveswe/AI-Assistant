a7c772a0107d15062ea858b5542ee00e7a3032f0
Clive Leddy <clive.leddy@gmail.com>
2026-05-27 09:43:54 +0200

    Created memory category model.

---
+++ Models/CategorizedMemory.cs
@@ -0,0 +1,9 @@
+﻿
+namespace LocalAiClient.Models;
+
+public class CategorizedMemory
+{
+    public string Category { get; set; } = "";
+
+    public List<ConversationMessage> Messages { get; set; } = [];
+}
